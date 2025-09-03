7fb60e505430f666e6e5e5ce69e5ba64dcf293ff
Lingxiao-LIU <linliu@augusta.edu>
2025-09-03 13:55:21 -0400

    Update __init__.py

--- hotspot/__init__.py
+++ hotspot/__init__.py
@@ -1,12 +1,9 @@
-name = "hotspot"
-
 from .hotspot import Hotspot
 
-# https://github.com/python-poetry/poetry/pull/2366#issuecomment-652418094
-# https://github.com/python-poetry/poetry/issues/144#issuecomment-623927302
 try:
     import importlib.metadata as importlib_metadata
 except ModuleNotFoundError:
     import importlib_metadata
-package_name = "hotspotsc"
+
+package_name = "hotspot"
 __version__ = importlib_metadata.version(package_name)
