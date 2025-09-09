3789e4017e4c10ab0482daeac6440b7769838295
Lingxiao-LIU <linliu@augusta.edu>
2025-09-09 19:50:25 -0400

    Delete hotspot directory

--- hotspot/__init__.py
+++
@@ -1,9 +0,0 @@
-from .hotspot import Hotspot
-
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
-
-package_name = "hotspot"
-__version__ = "0.1.0"
